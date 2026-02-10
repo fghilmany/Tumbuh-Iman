a258c3a83d809472e889bd6e7620d6f70c54c224
fghilmany <ceeros99@gmail.com>
2026-02-10 21:46:41 +0700

    feat(quran): Create a quran repository implement
    Refs #2

---
+++ lib/data/mapper/quran_mapper.dart
@@ -0,0 +1,28 @@
+import 'package:tumbuh_iman/data/models/quran/quran_list_response_model.dart';
+import 'package:tumbuh_iman/data/models/quran/surah_model.dart';
+import 'package:tumbuh_iman/domain/entities/quran/quran_entity.dart';
+import 'package:tumbuh_iman/domain/entities/quran/quran_item_entity.dart';
+
+extension QuranMapper on QuranListResponseModel {
+  QuranEntity toEntity() {
+    final listSurah = (data)
+        .map((item) => item.toEntity())
+        .toList();
+
+    return QuranEntity(
+      listSurah: listSurah,
+    );
+  }
+}
+
+extension SurahMapper on SurahModel {
+  QuranItemEntity toEntity() {
+    return QuranItemEntity(
+      id: number,
+      name: name,
+      numberOfAyah: numberOfAyah,
+      placeOfRevelation: placeOfRevelation,
+      meaning: meaning,
+    );
+  }
+}
---
+++ lib/data/repositories/quran/quran_repository_impl.dart
@@ -0,0 +1,36 @@
+import 'package:talker_flutter/talker_flutter.dart';
+import 'package:tumbuh_iman/core/utils/result.dart';
+import 'package:tumbuh_iman/data/mapper/quran_mapper.dart';
+import 'package:tumbuh_iman/data/remote/quran/quran_remote_data_source.dart';
+import 'package:tumbuh_iman/domain/entities/quran/quran_entity.dart';
+import 'package:tumbuh_iman/domain/repositories/quran/quran_repository.dart';
+
+class QuranRepositoryImpl implements QuranRepository {
+
+  final QuranRemoteDataSource _remoteDataSource;
+  final Talker _talker;
+
+  QuranRepositoryImpl(
+      this._remoteDataSource,
+      this._talker,
+      );
+
+  @override
+  Future<Result<QuranEntity>> getSurahList() async {
+    try {
+      _talker.debug('📖 Starting getSurahList in QuranRepositoryImpl...');
+
+      final response = await _remoteDataSource.getSurahList();
+      final quranEntity = response.toEntity();
+      _talker.info('✅ Successfully converted response to QuranEntity');
+      return Result.success(quranEntity);
+    } catch (e, stackTrace) {
+      _talker.error(
+        '❌ Error in getSurahList',
+        e,
+        stackTrace,
+      );
+      return Result.failure(e.toString());
+    }
+  }
+}
