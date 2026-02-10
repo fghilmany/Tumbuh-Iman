1f0ad30714c1849b07381645336d96770c32fb1c
fghilmany <ceeros99@gmail.com>
2026-02-10 11:48:12 +0700

    feat(quran): Add header icon Refs #2

---
+++ lib/presentation/features/quran/quran_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:tumbuh_iman/core/extensions/translations_extenstion.dart';
+import 'package:tumbuh_iman/gen/assets.gen.dart';
+import 'package:tumbuh_iman/presentation/components/cards/action_card.dart';
+import 'package:tumbuh_iman/presentation/components/cards/info_card.dart';
+import 'package:tumbuh_iman/presentation/components/misc/icon_custom.dart';
+import 'package:tumbuh_iman/presentation/components/misc/image_custom.dart';
+import 'package:tumbuh_iman/presentation/components/misc/badge.dart' as custom;
+import 'package:tumbuh_iman/presentation/components/misc/language_selector.dart';
+import 'package:tumbuh_iman/presentation/theme/app_colors.dart';
+import 'package:tumbuh_iman/presentation/theme/app_dimensions.dart';
+import 'package:tumbuh_iman/presentation/theme/app_text_styles.dart';
+
+class QuranScreen extends StatelessWidget {
+  const QuranScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.primaryVeryLight,
+      child: SafeArea(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned(
+              top: -30,
+              left: 0,
+              right: 0,
+              child: ImageCustom(
+                path: Assets.vector.header.path,
+                width: MediaQuery.sizeOf(context).width,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: AppDimensions.paddingM,
+                right: AppDimensions.paddingM,
+                top: AppDimensions.paddingM,
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: LanguageSelector(
+                      onLanguageChanged: (locale) {
+                        print('Language changed to: $locale');
+                      },
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("bismillah".tr, style: AppTextStyles.headline5),
+                      Text("next_prayer_times".trParams(
+                        {
+                          "0": "Dzuhur",
+                        }
+                      ), style: AppTextStyles.body2),
+                      Text("12:13:22", style: AppTextStyles.headline1),
+                      SizedBox(height: AppDimensions.spaceS),
+                      custom.Badge(
+                        text: "02:00 PM",
+                        textColor: AppColors.textPrimary,
+                        backgroundColor: AppColors.background.withValues(alpha: 0.75),
+                        prefix: Icon(
+                          Icons.access_time,
+                          size: AppDimensions.iconXS,
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      SizedBox(height: AppDimensions.spaceL),
+                      Row(
+                        children: [
+                          ActionCard(
+                            icon: Icons.history,
+                            title: 'last_read'.tr,
+                            color: AppColors.primary,
+                            onTap: () {},
+                          ),
+                          SizedBox(width: 12),
+                          ActionCard(
+                            icon: Icons.bookmark,
+                            color: AppColors.primary,
+                            title: 'bookmarks'.tr,
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: AppDimensions.spaceM),
+                      Flexible(
+                        child: SingleChildScrollView(
+                          child: ListView(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            children: List.generate(
+                              20,
+                              (index) =>  Padding(
+                                padding: const EdgeInsets.symmetric(vertical: AppDimensions.paddingS),
+                                child: InfoCard(
+                                  title: 'Last Read',
+                                  subtitle: 'Al-Fatihah, Ayah 5',
+                                  onTap: () {
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
